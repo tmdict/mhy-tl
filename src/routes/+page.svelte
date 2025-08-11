8015cf925c58fefa7a62a012a7ed4cfc490165a9
slsdo <gumugum@gmail.com>
2025-08-10 22:00:56 -0700

    5.8 update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,13 @@
       new: true,
       name: 'Character Builds',
       url: 'builds',
-      updated: '2025-06-22'
+      updated: '2025-08-10'
     },
     {
       new: true,
       name: 'Character Stat Scaling',
       url: 'character-scaling',
-      updated: '2025-06-22'
+      updated: '2025-08-10'
     },
     {
       name: 'Commission Achievements',
@@ -28,7 +28,7 @@
       new: true,
       name: 'Talent Materials',
       url: 'talent-materials',
-      updated: '2025-06-22'
+      updated: '2025-08-10'
     },
     {
       new: true,
@@ -37,10 +37,9 @@
       updated: '2025-06-22'
     },
     {
-      new: true,
       name: 'Weekly Boss Materials',
       url: 'weekly-boss-materials',
-      updated: '2025-06-22'
+      updated: '2025-08-10'
     }
   ].sort((a, b) => b.updated.localeCompare(a.updated));
 </script>
