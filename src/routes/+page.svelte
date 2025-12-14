726109b1b919e041cd25ddd02e2f5c88107b38d9
slsdo <gumugum@gmail.com>
2025-12-13 19:25:11 -0800

    6.2 update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,13 @@
       new: true,
       name: "Character Builds",
       url: "builds",
-      updated: "2025-10-26",
+      updated: "2025-12-13",
     },
     {
       new: true,
       name: "Character Stat Scaling",
       url: "character-scaling",
-      updated: "2025-10-26",
+      updated: "2025-12-13",
     },
     {
       name: "Commission Achievements",
