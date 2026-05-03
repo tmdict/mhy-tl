7f32caf14cf1df64aff8b8f668363f64bae48dfb
slsdo <gumugum@gmail.com>
2026-05-03 16:38:45 -0700

    update package

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import ViteYaml from "@modyfi/vite-plugin-yaml";
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
   plugins: [
     enhancedImages(),
     sveltekit(),
     ViteYaml(), // For loading yml
-    devtoolsJson(),
   ],
   server: { fs: { allow: ["data"] } },
 });
